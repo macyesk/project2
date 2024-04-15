fb5c69b52d7d85677aa5d12f565edca3ad35a135
FIRST_NAME LAST_NAME <kan99@byu.edu>
2024-04-15 15:03:20 -0600

    start of JS API

--- js/js1me.js
+++ js/js1me.js
@@ -1,15 +1,16 @@
 
 fetch("https://t3ogxvus80.execute-api.us-east-1.amazonaws.com/musicData")
-  .then((response) => response.json())
-  .then((data) => {
-    useData(data);
-    // function to execute when the data is retrieved
-  });
+    .then((response) => response.json())
+    .then((data) => {
+        useData(musicData);
+        // function to execute when the data is retrieved
+    });
 
-// JS for Table on Products page begins here
+// // JS for Table on Products page begins here
 
 // JS for Table on Products page ends here
-    // Loop through all table rows, and hide those who don't match the search query
+// Loop through all table rows, and hide those who don't match the search query
+function useData(musicData) {
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
         if (td) {
@@ -21,6 +22,46 @@ fetch("https://t3ogxvus80.execute-api.us-east-1.amazonaws.com/musicData")
             }
         }
     }
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+    const table = document.getElementById("myTable");
+    const input = document.getElementById("myInput");
+
+    input.addEventListener("keyup", function () {
+        const filter = input.value.toUpperCase();
+        fetch("https://t3ogxvus80.execute-api.us-east-1.amazonaws.com/musicData")
+            .then((response) => response.json())
+            .then((data) => {
+                // Clear table body before populating with new data
+                table.innerHTML = "";
+            }
 // JS for Table on Products page ends here
 
+
+// modal for news page
+// Get the modal element
+// const modal = document.getElementById("myModal");
+
+//         // Get the button that opens the modal
+//         const btn = document.getElementById("openModalBtn");
+
+//         // Get the <span> element that closes the modal
+//         const span = document.getElementsByClassName("close")[0];
+
+//         // When the user clicks the button, open the modal
+//         btn.addEventListener("click", function () {
+//             modal.style.display = "block";
+//         });
+
+//         // When the user clicks on <span> (x), close the modal
+//         span.addEventListener("click", function () {
+//             modal.style.display = "none";
+//         });
+
+//         // When the user clicks anywhere outside of the modal, close it
+// window.addEventListener("click", function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//         }
+//         })
