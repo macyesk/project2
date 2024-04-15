db977cfb616c335d6464ae36c10e14f1464de9a9
FIRST_NAME LAST_NAME <kan99@byu.edu>
2024-04-15 15:07:50 -0600

    api??

--- js/js1me.js
+++ js/js1me.js
@@ -28,40 +28,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const table = document.getElementById("myTable");
     const input = document.getElementById("myInput");
 
-    input.addEventListener("keyup", function () {
+    input.addEventListener("keyup", function (useData) {
         const filter = input.value.toUpperCase();
         fetch("https://t3ogxvus80.execute-api.us-east-1.amazonaws.com/musicData")
             .then((response) => response.json())
-            .then((data) => {
+            .then((musicData) => {
                 // Clear table body before populating with new data
                 table.innerHTML = "";
             }
 // JS for Table on Products page ends here
-
-
-// modal for news page
-// Get the modal element
-// const modal = document.getElementById("myModal");
-
-//         // Get the button that opens the modal
-//         const btn = document.getElementById("openModalBtn");
-
-//         // Get the <span> element that closes the modal
-//         const span = document.getElementsByClassName("close")[0];
-
-//         // When the user clicks the button, open the modal
-//         btn.addEventListener("click", function () {
-//             modal.style.display = "block";
-//         });
-
-//         // When the user clicks on <span> (x), close the modal
-//         span.addEventListener("click", function () {
-//             modal.style.display = "none";
-//         });
-
-//         // When the user clicks anywhere outside of the modal, close it
-// window.addEventListener("click", function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//         }
-//         })
